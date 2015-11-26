b4ed2f0495764fc58a4b791608a5eeb7d72b6192
Dmitriy Titarenko <mail.dscheg@gmail.com>
2015-11-27 00:02:21 +0500

    translator task

---
+++ tasks/transl/src/app.ts
@@ -0,0 +1,12 @@
+﻿window.onload = () => {
+	var input = <HTMLTextAreaElement>document.getElementById("input");
+	var output = document.getElementById("output");
+
+	var e2m = document.getElementById("e2m");
+	var e2mTranslator = Translator.english2martian();
+	e2m.onclick = () => output.textContent = e2mTranslator(input.value);
+
+	var m2e = document.getElementById("m2e");
+	var m2eTranslator = Translator.martian2english();
+	m2e.onclick = () => output.textContent = m2eTranslator(input.value);
+};
---
+++ tasks/transl/src/tests/tests.ts
@@ -0,0 +1,15 @@
+﻿/// <reference path="../app.ts"/>
+/// <reference path="../Scripts/typings/jasmine/jasmine.d.ts"/>
+
+describe("Translator", () => {
+	var e2m = Translator.english2martian();
+	it("should translate to martians", () => {
+		expect(e2m("Hello WORLD!!!")).toBe("○▭▱◜▶ ▥▻△◉◂▤▮▭");
+		expect(e2m("martian method knowledge error bad good")).toBe("◊▱◓▷◙▰◁ ▩○◌▷◓◜ ▷○◓▲▶□◇▪◀ ◄◒◍◌◛ ▢▩◎ ◑◊▵▱ ");
+		expect(e2m("good martian method knowledge error bad")).toBe("◓◌◓◜ ◓○●◅▹▼▹ ◓△◍◘▮▱ ▬□▭▦◊▣◘◎◄ ▥◉▣◜◒ ▼◉▱ ");
+	})
+
+	it("should translate to english", () => {
+		// TODO
+	});
+});
