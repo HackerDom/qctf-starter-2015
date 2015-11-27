0755681f6e075871efdb2c99e39b4881e980fc91
Andrew Gein <andgein@yandex.ru>
2015-11-28 02:14:27 +0500

    Neutrion building into backup.zip, remove recon flags, change urls to *.contest.qctf.ru

--- tasks/deploy_tasks.bat
+++ tasks/deploy_tasks.bat
@@ -1,5 +1,6 @@
 @echo off
 
+:projects
 echo ==== BUILD PROJECTS ====
 for /d %%d in (*) do (
     if exist %%d\prebuild.bat (
@@ -13,21 +14,32 @@ for /d %%d in (*) do (
 )
 
 
+:downloads
+
 echo ==== BUILD DOWNLOADS ====
+
+echo Zipping neutrino.zip...
+pushd neutro\archive
+if exist ..\neutrino.zip del ..\neutrino.zip
+"%~dp0tools\7z" a ..\neutrino.zip content meta
+popd
+
 
 echo Zipping translator.zip...
 
 pushd transl\src\
 if exist ..\translator.zip del ..\translator.zip
-%~dp0tools\7z a -x!obj -x!bin ..\translator.zip *
+"%~dp0tools\7z" a -x!obj -x!bin ..\translator.zip *
 popd
+
 
 echo Zipping backup.zip...
 
 pushd ..\checksystem\download\1b1baa8dbc68603a
 if exist backup.zip del backup.zip
-%~dp0tools\7z a backup.zip *.txt
-%~dp0tools\7z a backup.zip %~dp0transl\translator.zip
+"%~dp0tools\7z" a backup.zip *.txt
+"%~dp0tools\7z" a backup.zip "%~dp0transl\translator.zip"
+"%~dp0tools\7z" a backup.zip "%~dp0neutro\neutrino.zip"
 popd
 
 echo Copying add downloads to site...
