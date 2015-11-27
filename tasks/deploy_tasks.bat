14232c16be208ab2602cff6d5fd2d00247da5ac4
Andrew Gein <andgein@yandex.ru>
2015-11-27 18:09:11 +0500

    Add zipping to tasks deploy process

--- tasks/deploy_tasks.bat
+++ tasks/deploy_tasks.bat
@@ -1,5 +1,13 @@
+echo "Zipping backup.zip..."
+
+pushd ..\checksystem\download\1b1baa8dbc68603a
+%~dp0tools\zip backup.zip *.txt
+popd
+
+echo "Copying to site..."
 for /d %%d in (*) do (
 	xcopy /y /s %%d\deploy\* ..\checksystem\src\deploy\site\download\
 )
 
 xcopy /y /s ..\checksystem\download\* ..\checksystem\src\deploy\site\download\
+
